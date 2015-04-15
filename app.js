2028abad1b8470681db2b67164b21ab0dd78ecf2
Luke DAlfonso <pokemonmegaman@yahoo.com>
2015-04-14 23:50:19 -0500

    added serverside time syncing

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ app.get('/', function(req, res){
 
 //holds current video / video time
 var currenttime = 0;
+//Constant used to specify how often server should update playback time
+var TIMER_UPDATE_INTERVAL = 1000;
+
+setInterval(function(){
+  currenttime += TIMER_UPDATE_INTERVAL/1000;
+  io.emit('video time', currenttime);
+} , TIMER_UPDATE_INTERVAL);
 var currentvid = 'AvtYOLe6Gh8';
 
 //Handles socket events (TODO: Handling multiple connections from same host)
@@ -21,6 +28,8 @@ io.on('connection', function(socket){
   socket.on('video id', function(vid){
     //TODO: Add video to database for current playing video.
     currentvid = vid;
+    //Since new video played, set timer back to zero.
+    currenttime = 0;
     console.log('playing video with id ' + currentvid);
     io.emit('video id', currentvid);
   });
