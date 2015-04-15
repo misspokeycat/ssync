f4a680b0eea4e52cd6e182ca78e8caf8a647b733
Luke DAlfonso <pokemonmegaman@yahoo.com>
2015-04-15 16:19:50 -0500

    modified express code to serve entire static dir

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io').listen(http);
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
+var db = new sqlite3.Database(':memory:');  //Change to an actual file eventually!
+
+//SQLite code 
+db.serialize(function(){
+  db.run("CREATE TABLE IF NOT EXISTS playlist (id INTEGER PRIMARY KEY,title TEXT, yt_id TEXT, duration INTEGER)");
+  //Test add, but should be doable with other things
+  db.run("INSERT INTO playlist (title, yt_id, duration) VALUES (" +
+    "'beatmania IIDX - smooooch・∀・'" +", "+
+    "'QvGRj77EAOo'" +", "+
+    "120" + ")"); //Note that API data will be in the format PTxxMyyS
+  db.each("SELECT * FROM playlist", function(err, row){
+    //For now, just log everything in table playlist.
+    console.log(row);  //Appears this is returned as json, which ROCKS!
+  });
+}); 
+
+db.close(); 
 
 //Serve index.html page for request to /
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/static/index.html');
-});
+app.use('/', express.static(__dirname + "/static"));
 
 //holds current video / video time
 var currenttime = 0;
 //Constant used to specify how often server should update playback time
+//Eventually, have it so that server only sends playback time on request
 var TIMER_UPDATE_INTERVAL = 1000;
 
 setInterval(function(){
@@ -43,21 +59,7 @@ io.on('connection', function(socket){
   });
 });
 
-//SQLite code
-db.serialize(function(){
-  db.run("CREATE TABLE IF NOT EXISTS playlist (id INTEGER PRIMARY KEY,title TEXT, yt_id TEXT, duration INTEGER)");
-  //Test add, but should be doable with other things
-  db.run("INSERT INTO playlist (title, yt_id, duration) VALUES (" +
-    "'beatmania IIDX - smooooch・∀・'" +", "+
-    "'QvGRj77EAOo'" +", "+
-    "120" + ")");
-  db.each("SELECT * FROM playlist", function(err, row){
-    //For now, just log everything in table playlist.
-    console.log(row);  //Appears this is returned as json, which ROCKS!
-  });
-});
 
-db.close();
 //Starts the server
 http.listen(3000, function(){
   console.log('listening on *:3000')
